b6dc46cd885170a475e94d7de37c873bdf603e2e
YoimelDev <yoimeldev@gmail.com>
2023-03-01 19:14:16 -0400

    style: :lipstick: add transitions to articler card

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -6,25 +6,18 @@ const props = defineProps({
 
 <template>
   <div
-    class="w-[16.875rem] rounded-xl overflow-hidden drop-shadow-meat-shadow group transition-all"
-  >
+    class="w-[16.875rem] rounded-xl overflow-hidden drop-shadow-meat-shadow group transition-all duration-500 ease-out h-[400px] hover:h-[420px]">
     <img :src="article.image" width="270" height="200" alt="Image Preview" />
 
     <div class="bg-white px-[26px] py-9 transition-all">
-      <p
-        class="text-lg font-open-sans font-bold mb-2 group-hover:text-meat-yellow"
-      >
+      <p class="text-lg font-open-sans font-bold mb-2 group-hover:text-meat-yellow">
         {{ article.title }}
       </p>
       <p class="text-sm font-open-sans">
         {{ article.content }}
       </p>
 
-      <a
-        href="#"
-        class="uppercase mt-5 text-meat-yellow text-sm font-bold hidden group-hover:block"
-        >ver más</a
-      >
+      <a href="#" class="uppercase mt-5 text-meat-yellow text-sm font-bold hidden group-hover:block">ver más</a>
     </div>
   </div>
 </template>
