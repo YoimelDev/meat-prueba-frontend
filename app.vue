f84ea25bce48c31d730d2be8e0a0f8514c29b84f
Yoimel Rojas <yoimeldev@gmail.com>
2023-03-01 13:46:26 -0400

    style: :lipstick: add section contact us

--- app.vue
+++ app.vue
@@ -57,7 +57,7 @@ import ArticleCard from "./components/ArticleCard";
     </div>
   </div>
 
-  <main class="md:px-32">
+  <main class="md:px-32 mb-10">
     <section>
       <div class="flex flex-col items-center md:w-[640px] mx-auto mt-10 mb-20">
         <h2
@@ -90,6 +90,75 @@ import ArticleCard from "./components/ArticleCard";
           <ArticleCard />
         </div>
       </div>
+    </section>
+
+    <section>
+      <div class="flex flex-col items-center md:w-[640px] mx-auto mt-10 mb-12">
+        <h2
+          class="font-caveat font-bold text-meat-gray text-4xl md:text-8xl text-center relative after:content-[' '] after:bg-brush after:absolute after:left-0 after:bottom-[-35px] after:w-full after:h-20 after:bg-cover after:opacity-20"
+        >
+          Contáctanos
+        </h2>
+      </div>
+
+      <form class="max-w-[720px] mx-auto">
+        <div class="flex flex-wrap gap-5 justify-between">
+          <div>
+            <label class="font-bold font-open-sans text-xs" for="name"
+              >NOMBRE</label
+            >
+            <input
+              id="name"
+              type="text"
+              class="bg-white mt-2 border border-[#CFCFCF] w-[350px] text-[#3F4544] text-sm focus:outline-none focus:border-meat-yellow block px-[22px] py-3"
+              required
+            />
+          </div>
+          <div>
+            <label class="font-bold font-open-sans text-xs" for="lastname"
+              >APELLIDO</label
+            >
+            <input
+              id="lastname"
+              type="text"
+              class="bg-white mt-2 border border-[#CFCFCF] w-[350px] text-[#3F4544] text-sm focus:outline-none focus:border-meat-yellow block px-[22px] py-3"
+              required
+            />
+          </div>
+
+          <div>
+            <label class="font-bold font-open-sans text-xs" for="mail"
+              >MAIL</label
+            >
+            <input
+              id="mail"
+              type="email"
+              class="bg-white mt-2 border border-[#CFCFCF] w-[350px] text-[#3F4544] text-sm focus:outline-none focus:border-meat-yellow block px-[22px] py-3"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              class="font-bold font-open-sans text-xs uppercase"
+              for="phone"
+              >téléfono</label
+            >
+            <input
+              id="phone"
+              type="tel"
+              class="bg-white mt-2 border border-[#CFCFCF] w-[350px] text-[#3F4544] text-sm focus:outline-none focus:border-meat-yellow block px-[22px] py-3"
+              required
+            />
+          </div>
+        </div>
+
+        <button
+          class="block ml-auto mt-8 rounded-full bg-meat-yellow px-12 py-4 text-sm font-bold font-open-sans text-white hover:bg-meat-blue disabled:bg-[#CFCFCF]"
+        >
+          ENVIAR
+        </button>
+      </form>
     </section>
   </main>
 </template>
